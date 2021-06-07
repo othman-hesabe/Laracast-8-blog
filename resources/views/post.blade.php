c4c6a414b9b52ad6d853cdbd704f579160ac993b
Shahbaaz Rokadia <srokadia@hesabe.com>
2021-06-07 11:11:21 +0300

    update: renamed blade view file and rm js file in public

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,6 @@
 
 <title>My Blog</title>
 <link rel="stylesheet" href="/app.css">
-<script src="/app.js"></script>
 
 <body>
     <h1>Hello World</h1>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('post');
     // return "Hello";
     // return ['foo' => 'bar'];
 });
