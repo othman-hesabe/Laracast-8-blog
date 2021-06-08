abe9a86fc4e4bd6efa3e0578578b248c64af6eff
Shahbaaz Rokadia <srokadia@hesabe.com>
2021-06-08 23:38:11 +0300

    created new variable called POST and return Hello World from web.php route function

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,7 @@
 <body>
     <article>
         <h1><a href="/post">My First Post</a></h1>
-
+        <?= $post ?>
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus totam dolores, magnam porro aliquid
             quos nisi explicabo ducimus odio fugiat adipisci tempore tempora ea odit cumque sint officiis, corrupti
