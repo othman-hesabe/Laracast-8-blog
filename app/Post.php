f3756cc02af164389711ea0b1d993a161bb587a9
Othman Othman <othman@hesabe.com>
2021-06-16 10:01:14 +0300

    Fillable and Guarded properties introduced. Each have their own use cases and how flexible the stakeholder wants the data security to be.

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+//    use HasFactory;
+
+//    Guarded Property
+//    Everything can be edited except for the params and in this case ['id'] is protected.
+    protected $guarded = [];
+
+//    Fillable Property
+//    Everything included in the param is vulnerable to being edited except the ones not mentioned.
+//    In this case  ['title', 'excerpt', 'body', 'id'] are the exception of edit-able params.
+//    protected $fillable = ['title', 'excerpt', 'body', 'id'];
 }
