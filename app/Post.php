b7cc97a9df061185cf71e6d0ee82cf887837f726
Othman Othman <othman@hesabe.com>
2021-06-16 14:27:25 +0300

    created categories for each post

--- app/Post.php
+++ app/Post.php
@@ -21,4 +21,9 @@ class Post extends Model
     {
         return 'slug';
     }
+
+    public function category(){
+//        hasOne, hasMany, belongsTo, belongsToMany
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,11 @@
             {!! $post->title !!}
         </h1>
 
+        <p>
+            <a href="#">{{$post->category->name}}</a>
+        </p>
+
+
         <div>
             {!! $post->body !!}
         </div>
